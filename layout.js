5ba89cd20bfdf3f8e6de07eb06924ec1edc28a74
anthony.wong <donghao.wung@gmail.com>
2021-04-12 22:38:04 +0800

    compute main axis

--- layout.js
+++ layout.js
@@ -8,7 +8,7 @@ function getStyle(element) {
 
 		if (element.style[prop].toString().match(/px$/)) {
 			element.style[prop] = element.computedStyle[prop].value
-		}  
+		}
 
 		if (element.style[prop].toString().match(/^[0-9\.]+$/)) {
 			element.style[prop] = parseInt(element.style[prop])
@@ -167,7 +167,88 @@ function layout(element) {
 	}
 	flexLine.mainSpace = mainSpace
 
-	console.log(items)
+	if (style.flexWrap === 'nowrap' || isAutoMainSize) {
+		flexLine.crossSpace = (style[crossSize] !== undefined) ? style[crossSize] : crossSpace
+	} else {
+		flexLine.crossSpace = crossSpace
+	}
+
+	if (mainSpace < 0) {
+		var scale = style[mainSize] / (style[mainSize] - mainSpace)
+		var currentMain = mainBase
+
+		for (var i = 0; i < items.length; i++) {
+			var item = items[i]
+			var itemStyle = getStyle[item]
+
+			if (itemStyle.flex) {
+				itemStyle[mainSize] = 0
+			}
+
+			itemStyle[mainSize] *= scale
+			itemStyle[mainStart] = currentMain
+			itemStyle[mainEnd] = itemStyle[mainStart] + mainSign * itemStyle[mainSize]
+			currentMain = itemStyle[mainEnd]
+		}
+	} else {
+		flexLine.forEach(function(items) {
+			var mainSpace = items.mainSpace
+			var flexTotal = 0
+			for (var i =0; i < items.length; i++) {
+				var item = items[i]
+				var itemStyle = getStyle(item)
+
+				if ((itemStyle.flex !== null) && (itemStyle.flex !== (void 0))) {
+					flexTotal += itemStyle.flex
+					continue
+				}
+			}
+
+			if (flexTotal > 0) {
+				// There is flexible flex items
+				var currentMain = mainBase
+				for (var i = 0; i < items.length; i++) {
+					var item = items[i]  
+					var itemStyle = getStyle(item)
+
+					if (itemStyle.flex) {
+						itemStyle[mainSize] = (mainSpace / flexTotal) * itemStyle.flex
+					}
+					itemStyle[mainStart] = currentMain
+					itemStyle[mainEnd] = itemStyle[mainStart] + mainSign * itemStyle[mainSize]
+					currentMain = itemStyle[mainEnd]
+				}
+			} else {
+				// There is No flexible flex items, which means justifyContent should work
+				if (style.justifyContent === 'flex-start') {
+					var currentMain = mainBase
+					var step = 0
+				}
+				if (style.justifyContent === 'flex-end') {
+					var currentMain = mainSpace * mainSign + mainBase
+					var step = 0
+				}
+				if (style.justifyContent === 'center') {
+					var currentMain = mainSpace / 2 * mainSign + mainBase
+					var step = 0
+				}
+				if (style.justifyContent === 'space-between') {
+					var step = mainSpace / (items.length - 1) * mainSign
+					var currentMain = mainBase
+				}
+				if (style.justifyContent === 'space-around') {
+					var step = mainSpace / items.length * mainSign
+					var currentMain = step / 2 + mainBase
+				}
+				for (var i = 0; i < items.length; i++) {
+					var item = items[i]
+					itemStyle[mainStart] = currentMain
+					itemStyle[mainEnd] = itemStyle[mainStart] + mainSign * itemSize[mainSize]
+					currentMain = itemStyle[mainEnd] + step
+				}
+			}
+		})
+	}
 }
 
 module.exports = layout
